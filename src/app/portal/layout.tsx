c7df0affa4c857567a21321ebb3f7bf3e8238184
Macgayver Rosa <macgayver.rosa@outlook.com>
2025-08-07 21:44:12 +0800

    adding session check

--- src/app/portal/layout.tsx
+++ src/app/portal/layout.tsx
@@ -1,11 +1,17 @@
+import { auth } from "@/auth";
 import Footer from "@/components/layout/global/footer";
 import { NavBar } from "@/components/layout/global/navbar";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  const session = await auth();
+  if (!session) {
+    redirect("/sign-in");
+  }
   return (
     <>
       <NavBar />
