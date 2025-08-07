2e31e627b26549ea6f0d5b6af48fcb616f38544a
Macgayver Rosa <macgayver.rosa@outlook.com>
2025-08-07 20:34:15 +0800

    updating the global config

--- src/config/constants/global-config.ts
+++ src/config/constants/global-config.ts
@@ -1,20 +1,11 @@
 export const GlobalConfig = {
-  APP_NAME: "GMT",
+  APP_NAME: "SellMyCar",
+  APP_NAME_FIRST: "SellMy",
+  APP_NAME_LAST: "Car",
   APP_VERSION: "1.0.0",
-  APP_DESCRIPTION: "Trades Wise is a powerful tool for managing your tasks and projects.",
-  APP_AUTHOR: "Your Name",
-  APP_COPYRIGHT: "© 2023 Your Name. All rights reserved.",
-  APP_LOGO: "/logo.png",
-  APP_LOGO_DARK: "MyApp Logo",
+  APP_DESCRIPTION: "Description",
+  APP_AUTHOR: "ESC Company",
   APP_FAVICON: "/favicon.ico",
-  // APP_LANGUAGE: "en-US",
-  // APP_COUNTRY: "US",
-  // APP_CURRENCY: "USD",
-  // APP_TIMEZONE: "America/New_York",
-  // APP_DATE_FORMAT: "MM/DD/YYYY",
-  // APP_TIME_FORMAT: "12-hour", // or "24-hour"
-  // APP_LOCALE: "en-US",
-  // APP_API_URL: "https://api.myapp.com",
 
   // APP Socials
   SOCIAL_Facebook: "#",
