b9e89a1336ac50240913fc2b13b976efa9a7d09e
Macgayver Rosa <macgayver.rosa@outlook.com>
2025-08-07 21:54:13 +0800

    updating logo

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,7 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
-import { GalleryVerticalEnd } from "lucide-react";
 import { LoginForm } from "@/components/layout/global/auth/login-form";
+import { IconsLogo } from "@/components/shared/icons/icons-logo";
 
 export default async function LoginPage() {
   const session = await auth();
@@ -12,12 +12,7 @@ export default async function LoginPage() {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Acme Inc.
-          </a>
+          <IconsLogo.text />
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,5 @@
-import { auth } from "@/auth"
-import { NextResponse } from 'next/server'
+export { auth as middleware } from "@/auth"
 
-export default auth((req) => {
-  // If the user is not authenticated, redirect them to the sign-in page.
-  if (!req.auth) {
-    const signInUrl = new URL('/sign-in', req.nextUrl.origin)
-    return NextResponse.redirect(signInUrl)
-  }
-})
-
-// Your config remains the same.
 export const config = {
   matcher: ["/portal/:path*"],
 }
