6a052a7e993a3ae0234d068bdf881c06b1a00b14
Macgayver Rosa <macgayver.rosa@outlook.com>
2025-08-07 20:48:41 +0800

    eslint

--- src/components/layout/global/navbar/components/navbar-auxbar.tsx
+++ src/components/layout/global/navbar/components/navbar-auxbar.tsx
@@ -1,5 +1,4 @@
 import { Separator } from "@/components/ui/separator";
-// import SiteFlags from "@/components/shared/icons/icons-flags";
 import SocialIcons from "@/components/shared/icons/icons-socials";
 import { MaxWidthWrapper } from "@/components/shared/wrappers/max-width-wrapper";
 import IconsFlag2 from "@/components/shared/icons/icons-flags2";
@@ -11,21 +10,6 @@ export default function NavbarAuxBar() {
         <div className="flex md:grid md:grid-cols-12 h-10 items-center justify-between border-none">
           <div className="hidden md:flex md:col-span-6 text-white">
             <SocialIcons hover="text-foreground" />
-            {/* <div className="flex gap-2">
-
-              <Link href="#" className="hover:">
-                <Icons.facebook className="h-5 w-5" />
-              </Link>
-              <Link href="#" className="hover:text-foreground">
-                <Icons.instagram className="h-5 w-5" />
-              </Link>
-              <Link href="#" className="hover:text-foreground">
-                <Icons.linkedin className="h-5 w-5" />
-              </Link>
-              <Link href="#" className="hover:text-foreground">
-                <Icons.twitter className="h-5 w-5" />
-              </Link>
-            </div> */}
           </div>
           <div className="flex items-center gap-x-2 ms-auto md:col-span-6 text-white text-sm">
             Placeholder 1
@@ -36,8 +20,7 @@ export default function NavbarAuxBar() {
               />
 
 
-            {/* <SiteFlags /> */}
-            <IconsFlag2 />
+            {/* <IconsFlag2 /> */}
           </div>
         </div>
       </MaxWidthWrapper>
